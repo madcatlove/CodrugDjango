3aad53e1e7888bc5435ac8ce8f13d6a471b144db
madcat <kr.madcat@gmail.com>
2014-11-02 10:14:14 +0900

    STATIC File DIR TEMPLATE FILE DIR T.T

--- CodrugWWW/models.py
+++ CodrugWWW/models.py
@@ -1,14 +1,9 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 
-'''
-    Member Model
-'''
 class Member(models.Model):
 
-    '''
-        Member Field
-    '''
     email = models.CharField(max_length = 150)
     password = models.CharField(max_length = 100)
     name = models.CharField(max_length = 30)
