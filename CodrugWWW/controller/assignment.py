ffec8a0b1484b2c2d71cbd8ef52ce57fc453be4d
김재현 <nobjob@naver.com>
2014-11-12 18:43:05 +0900

    1

--- CodrugWWW/controller/assignment.py
+++ CodrugWWW/controller/assignment.py
@@ -11,20 +11,36 @@ import re
 import math
 from ..models import *
 
+'''
+    과제 리스트 컨트롤러
+'''
 
 def assignment_list(request):
 
     assignmentList = Assignment.objects.all()
-
+    # 파일이 존재하면 이미지, 기타파일 분류작업.
+    oImg = []
+    oEtc = []
+    for x in assignmentList:
+        if x.image_ref > 0:
+            oFile = File.objects.filter(id=x.image_ref)
+            # 파일 분류작업
+            for each in oFile:
+                if re.search( r'\.(jpg|png|bmp)$', str(each.outFILE)):
+                    oImg.append(each)
+                else:
+                    oEtc.append(each)
     rContext = RequestContext(request)
     ctx = {
-        'aList' : assignmentList
+        'aList' : assignmentList,
+        'imgList' :oImg,
+        'flieList':oEtc,
     }
     rend = render(request, 'assignment.html', ctx, context_instance=rContext)
     return HttpResponse( rend )
 
 '''
-    과제 제출 컨트롤러
+    과제 생성 컨트롤러
 '''
 def assignment_write(request):
 
@@ -40,3 +56,5 @@ def assignment_write(request):
 '''
 def assignment_detail(request, articleId):
     pass
+
+
