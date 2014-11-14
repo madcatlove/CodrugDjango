5c4cd843dc510a03d5d17d6bda92e7007b095376
madcat <kr.madcat@gmail.com>
2014-11-14 19:12:11 +0900

    회원 수정 페이지/ 컨트롤러 (작동완벽 ㅋ )

--- CodrugDjango/settings.py
+++ CodrugDjango/settings.py
@@ -37,8 +37,8 @@ else:
 SECRET_KEY = '%cvxpxlmwv=8#yn))(84wm6cgk5ox520-2emcws06i96_5qe+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-TEMPLATE_DEBUG = True
+DEBUG = (isProduction == False)
+TEMPLATE_DEBUG = (isProduction == False)
 
 TEMPLATE_DIRS = ( BASE_DIR + '/templates', )
 
